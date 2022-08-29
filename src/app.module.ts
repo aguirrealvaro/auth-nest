a6c8a2f9b4d368b83021ed179aa534254e59f63d
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-08-29 16:24:44 -0300

    Create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,8 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { ConfigurationModule } from "./config/config.module";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: `${process.cwd()}/.env.${process.env.NODE_ENV}`,
-      isGlobal: true,
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigurationModule, AuthModule],
 })
 export class AppModule {}
